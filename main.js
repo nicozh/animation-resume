3ee30a3fc56d37b9ad784dd5044e8e4b471c8599
zhang <mitmiky@outlook.com>
2018-02-02 23:48:41 +0800

    add markdown

--- main.js
+++ main.js
@@ -1,71 +1,97 @@
 var result = `/*
-  *  面试官你好，我是张洪
-  *  我将以动画的形式介绍自己
-  *  只用文字介绍太单调了
-  *  那就边写代码边自我介绍吧
-  *  首先我要给页面加一些样式
-*/
+    *  面试官你好，我是张洪
+    *  我将以动画的形式介绍自己
+    *  只用文字介绍太单调了
+    *  那就边写代码边自我介绍吧
+    *  首先我要给页面加一些样式
+  */
+  
+  *{  
+      transition: all 1s;
+  }
+  body{
+      background: #002B36;
+      font-size:15px;
+  }
+  #code{
+      padding:20px;
+      border:1px solid green;
+      animation: breath 0.5s infinite alternate-reverse;
+  }
+  `
 
-*{
-    transition: all 1s;
+var resultTwo = `
+.page{
+    border:1px solid red;
+    float:left;
+    width:45%;
+    margin:20px;
+    overflow: scroll;
+    height:100vh;
 }
-body{
-    background: pink;
-    font-size:15px;
-}
-#code{
-    padding:20px;
-    border:3px solid green;
-}
-`
-var styleTag = document.querySelector('#style')
-var code = document.querySelector('#code')
+    `
 
-var x = 0
-var id = setInterval(() => {
-    styleTag.innerHTML = result.slice(0, x)
-    code.innerHTML = result.slice(0, x)
-    code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css);  //代码高亮
-    x += 1
-    if (x > result.length) {
-        window.clearInterval(id)
-        fn()
-        fn1(result)
-    }
-}, 100)
+var md = `
+   # 开始写简历
+    `
 
-function fn() {
+function writeCode(codefix, code, fn) {
+    let styleTag = document.querySelector('#style')
+    let domCode = document.querySelector('#code')
+    // domCode.innerHTML = result || ''
+    let x = 0
+    let id = setInterval(() => {
+        domCode.scrollTop = 10000
+        styleTag.innerHTML = codefix + code.slice(0, x)
+        domCode.innerHTML = Prism.highlight(codefix + code.slice(0, x), Prism.languages.css);  //代码高亮
+        x += 1
+        if (x > code.length) {
+            window.clearInterval(id)
+            fn.call()
+        }
+    }, 100)
+
+}
+
+function writeMarkdown(md) {
+    let markdown = document.querySelector('#content')
+    let x = 0
+    let id = setInterval(() => {
+        //解析markdown
+        var rendererMD = new marked.Renderer();
+        marked.setOptions({
+            renderer: rendererMD,
+            gfm: true,
+            tables: true,
+            breaks: false,
+            pedantic: false,
+            sanitize: false,
+            smartLists: true,
+            smartypants: false
+        });//基本设置
+        // markdown.innerHTML = md.slice(0, x) 
+        markdown.innerHTML = marked(md.slice(0, x))
+        x += 1
+        if (x > code.length) {
+            window.clearInterval(id)
+        }
+    }, 100)
+}
+
+function fn(fn2) {
     var w = document.querySelector('.window')
     var div = document.createElement('div')
+    var pre = document.createElement('pre')
+    pre.id = 'content'
     div.id = 'page'
     div.className = 'page'
     w.appendChild(div)
+    div.appendChild(pre)
+    fn2()
 }
 
-function fn1(resultTwo) {
-    let result = `
-.page{
-    border:3px solid red;
-    float:left;
-    width:45%;
-    margin:0 20px;
-    overflow:auto;
-    border-radius:10px;
-}
-#page{
-    height:500px;
-}
-    `
-    n=0
-    id=setInterval(()=>{
-        n+=1
-        code.innerHTML = resultTwo + result.slice(0, n)
-        code.innerHTML = Prism.highlight(code.innerHTML, Prism.languages.css);  //代码高亮
-        styleTag.innerHTML = resultTwo + result.slice(0, n)
-        if(n>result.length){
-            clearInterval(id)
-        }
-    },100)
-}
-
-
+writeCode('', result, () => {
+    fn(() => {
+        writeCode(result, resultTwo, () => { console.log(1); writeMarkdown(md) })
+    })
+})
